aed00fb5e5a984a999373785f80281b77ae574e8
uzairhassan <uzairh.iflume@outlook.com>
2018-02-17 18:20:39 +0500

    change

--- get_domains.py
+++ get_domains.py
@@ -42,59 +42,58 @@ for item in data:
     #         if "http" not in words and "https" not in words:
     #             filtered_content+=words+" "
     if "urdu.dawn.com" not in url:
-        try:
-            url_open_output = urllib.urlopen(url)
-            page = url_open_output.read()
-            soup = BeautifulSoup(page, "html.parser")
-            span_domain = soup.find('li', class_="active")
-            div_content = soup.findAll('div', class_="story__content pt-1 mt-1")
-            table = soup.findAll('div' , attrs={"class": "story__content pt-1 mt-1"})
-            contentt = None
-            final_string = ''
-            if span_domain is not None and div_content is not None:
-                # print "Entered"
-                for x in table:
-                    contentt = x.contents
-                    for element in contentt:
-                        if element.name is not None:
-                            if element.name.encode('utf-8') == "p":
-                                for details in element.contents:
-                                    # print type(details)
-                                    if details is not None and details.name is not None and details.name.encode(
-                                            'utf-8') != 'div':
-                                        # print details
-                                        final_string += details.text + " "
-                                    elif details.name is None:
-                                        # print 'Detail was None'
-                                        # print details
-                                        final_string += details + " "
+        url_open_output = urllib.urlopen(url)
+        page = url_open_output.read()
+        soup = BeautifulSoup(page, "html.parser")
+        span_domain = soup.find('li', class_="active")
+        div_content = soup.findAll('div', class_="story__content pt-1 mt-1")
+        table = soup.findAll('div' , attrs={"class": "story__content pt-1 mt-1"})
+        contentt = None
+        final_string = ''
+        if span_domain is not None and div_content is not None:
+            # print "Entered"
+            for x in table:
+                contentt = x.contents
+                for element in contentt:
+                    if element.name is not None:
+                        if element.name.encode('utf-8') == "p":
+                            for details in element.contents:
+                                # print type(details)
+                                if details is not None and details.name is not None and details.name.encode(
+                                        'utf-8') != 'div':
+                                    # print details
+                                    final_string += details.text + " "
+                                elif details.name is None:
+                                    # print 'Detail was None'
+                                    # print details
+                                    final_string += details + " "
 
-                                            # print 'printing final string'
-                if final_string != "":
-                    final_string = final_string.replace('\n',' ')
-                    domain = span_domain.a.string
-                    domain = domain.lower()
-                    print domain + "   " + final_string
-                    identifier = item['id_str']
-                    t = {
-                        "text": final_string ,
-                        "class": domain ,
-                        "id_str": identifier
-                    }
-                    count += 1
-                    processed_count += 1
-                    print str(count) + " Valid"
-                    domain_dict.append(t)
-                    time.sleep(3)
-                else:
-                    print "String was empty"
-
-
-
+                                    # print 'printing final string'
+            if final_string != "":
+                final_string = final_string.replace('\n',' ')
+                domain = span_domain.a.string
+                domain = domain.lower()
+                print domain + "   " + final_string
+                identifier = item['id_str']
+                t = {
+                    "text": final_string ,
+                    "class": domain ,
+                    "id_str": identifier
+                }
+                count += 1
+                processed_count += 1
+                print str(count) + " Valid"
+                domain_dict.append(t)
+                time.sleep(3)
             else:
-                continue
-        except:
-            print "unable to open "+url
+                print "String was empty"
+
+
+
+        else:
             continue
+        # except:
+        #     print "unable to open "+url
+        #     continue
 
 print "total tweets "+ str(len(domain_dict))
